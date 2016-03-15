3380e51dd2ef6b206680f75c0a979121f0c4607e
NicoleMann <zukoess7@gmail.com>
2016-03-15 09:53:31 -0400

    nav edit 6

--- GitHub/js/getNav.js
+++ GitHub/js/getNav.js
@@ -2,4 +2,4 @@ $.get("http://NicoleMann.github.io/Github/partials/nav.html",function(data){
                                                                        $(document).ready(function(){
     $(".container").prepend(data)
 });
-                                                                       }));
+                                                                       });
