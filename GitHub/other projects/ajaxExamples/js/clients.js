f0adc4926c3a9d57e4e7e6ce5dbae31b92222ce0
NicoleMann <zukoess7@gmail.com>
2016-03-23 16:11:28 -0400

    general edis

--- GitHub/other projects/ajaxExamples/js/clients.js
+++ GitHub/other projects/ajaxExamples/js/clients.js
@@ -1,6 +1,6 @@
 $(document).ready(function(){
     $("#getClients").click(function(){
-        var url = "../jsonDatabase/clients.json";
+        var url = "http://NicoleMann.github.io/GitHub/other projects/ajaxExamples/jsonDatabase/clients.json";
         $.getJSON(url, function(data){
             var html = "<table>" + "<tr><th>Name</th><th>Email</th><th>Company</th></tr>";
             $.each(data, function(index, item){
