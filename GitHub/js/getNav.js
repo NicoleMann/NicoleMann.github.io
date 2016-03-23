2d50d1d1369574a827de9af3e569b5b7be8f7979
NicoleMann <zukoess7@gmail.com>
2016-03-23 12:32:43 -0400

    even more nav edits

--- GitHub/js/getNav.js
+++ GitHub/js/getNav.js
@@ -1,8 +1,6 @@
 $.get("http://NicoleMann.github.io/Github/partials/nav.html",function(data){
     $(document).ready(function(){
-        var container = $(".container");
-        if (container){
-            container.prepend(data)
-        }
+        $(".container").prepend(data);
+        $(".container").fadeIn();
     });
 });
