af513bd1d8aa48af34b57c56f638930a04b3a788
NicoleMann <zukoess7@gmail.com>
2018-06-18 15:35:55 +0000

    Update storybuilder.js

--- root/storyBuilder/storybuilder.js
+++ root/storyBuilder/storybuilder.js
@@ -1,6 +1,6 @@
 //trying to convert broken PHP forms to entirely javascript game that actually WORKS
 
-$(document).ready(function(){ //THIS IS JQUERY, PLEASE FIX
+$(document).ready(function(){ //OKAY WE'RE LEAVING IT JQUERY
 
   var protag;
   var male = Document.getElementById("male");
