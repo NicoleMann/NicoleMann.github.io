5abfa3c7d608422f7bb2e1232d8e2beb24f593f3
NicoleMann <zukoess7@gmail.com>
2018-06-18 15:33:07 +0000

    GETTING THIS PARTY STARTED

--- root/storyBuilder/storybuilder.js
+++ root/storyBuilder/storybuilder.js
@@ -1,1 +1,10 @@
 //trying to convert broken PHP forms to entirely javascript game that actually WORKS
+
+$(document).ready(function(){ //THIS IS JQUERY, PLEASE FIX
+
+  var protag;
+  var male = Document.getElementById("male");
+  if(male.clicked){
+  protag = male; //HOW DO I MAKE YOU REMEMBER THAT YOU'RE MALE NOW??
+  }
+});
